220bf5d79a7a7b09f214739b12dae77303eeeeb4
Hunter <HunterNL@users.noreply.github.com>
2024-06-01 21:47:12 +0200

    i love adding print statements to only see the result in 5 minutes plz why

--- src/api.rs
+++ src/api.rs
@@ -134,6 +134,7 @@ const HTTP_CACHE_LINK_PATH: &str = "links.json";
 
 pub fn serve(config: &AppConfig, autofetch: bool) -> Result<(), anyhow::Error> {
     if autofetch {
+        println!("Autofetching...");
         fetch::fetch(config)?
     }
 
